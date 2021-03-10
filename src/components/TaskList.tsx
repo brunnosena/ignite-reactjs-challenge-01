a348c9b803195caf12f68fb66cd77d289251df4d
Brunno Sena <brunnow@hotmail.com>
2021-03-10 18:06:35 -0300

    challenge finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (newTaskTitle === '') return;
+
+    const ceratedTaks = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    };
+
+    setTasks(previousTasks => [...previousTasks, ceratedTaks]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const newTaks = tasks.map(task =>
+      task.id === id ? { ...task, isComplete: !task.isComplete } : task,
+    );
+
+    setTasks(newTaks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const newTaks = tasks.filter(task => task.id !== id);
+
+    setTasks(newTaks);
   }
 
   return (
